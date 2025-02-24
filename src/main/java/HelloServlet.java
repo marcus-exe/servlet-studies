cc1bf134adea0895ccf6d21648edffdb3e543acd
Marcus Sena <mens.eai20@uea.edu.br>
2025-02-24 06:02:37 -0400

    first commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    public void init(ServletConfig config)
+            throws ServletException {}
+
+    public void destroy() {}
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Tomcat Servlet!</h1>");
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {}
+}
